020848eec0dd79935eaf0473f1e8c54d39d9de75
davidtiemstra <79322999+davidtiemstra@users.noreply.github.com>
2022-12-23 14:21:18 +0100

    Update randomwalker.py

--- randomwalker.py
+++ randomwalker.py
@@ -1,6 +1,7 @@
 import jefgenerator
 import noise
 import math
+import antdrawer
 
 # do randomwalk (keep in same file for now)
 
@@ -8,11 +9,16 @@ import math
 # noise.snoise2(i, 0, 4, 5.0, 0.40)
 # right now i have about 3 shifts over 42 points so lets say 15 per thing. decrease lacunarity to make it shift slower
 
-for n in range(50):
+stepsize = 40
+antscale = 1.5
+
+for n in range(60):
     steps = []
     direction = 0.0
     for i in range(100):
         direction += noise.snoise2(50*n + i, 0, 4, 5.0, 0.40,) * 0.75
-        steps.append([round(10 * math.cos(direction)), round(10 * math.sin(direction))])
-        
+        steps.append([round(stepsize * math.cos(direction)), round(stepsize * math.sin(direction))])
+    
+    steps += antdrawer.antdrawer(direction - 0.5 * math.pi, antscale)
+
     jefgenerator.export_jef(steps, 'randomwalk' + str(n) + '.jef')
