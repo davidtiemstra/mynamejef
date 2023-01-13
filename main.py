ed4b4996b0ab150da82986250f64f92668e0e979
semallush <semallush@gmail.com>
2023-01-13 16:17:17 +0100

    added test code to test pathviewer to main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-
+from helpers import pathviewer
+import random
 
 if __name__ == "__main__":
-    print("I can now display paths!")
+
+    viewer = pathviewer.PathViewer(primary_color='blue', background_color='white')
+    #viewer.primary_color = 'blue'
+
+    path = []
+    for i in range(100):
+        x = random.randint(0,700)
+        y = random.randint(0,700)
+        path.append([x,y])
+
+    viewer.render_path(path, pathviewer.DisplayMode.POINTS)
+    viewer.update()
 
