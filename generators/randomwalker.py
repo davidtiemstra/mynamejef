bf2e97cc12da70db5b1fc9bf8bdfb14cc5d49d86
davidtiemstra <dntiemstra@hotmail.com>
2023-04-02 03:12:45 +0200

    added wonkycircle, made satinstich more accurate

--- generators/randomwalker.py
+++ generators/randomwalker.py
@@ -6,9 +6,9 @@ from generators import antdrawer
 
 # do randomwalk (keep in same file for now)
 
-# snoise2: [x, y, octaves, persistence, lacunarity]
-# noise.snoise2(i, 0, 4, 5.0, 0.40)
+# noise.snoise2(x=i, y=0, octaves=4, persistence=5.0, lacunarity=0.40)
 # right now i have about 3 shifts over 42 points so lets say 15 per thing. decrease lacunarity to make it shift slower
+# find full noise explanation either with help() or in pnoise-doc.txt
 
 def randomWalkAnt():
     stepsize = 40
@@ -33,7 +33,7 @@ def randomwalker(length, stepsize=50, direction=0.0, seed=False, absolute=False)
         seed = 1000 * random.random()
 
     for i in range(length):
-        direction += noise.snoise2(seed+i, 0, 4, 5.0, 0.40,) * 0.75
+        direction += noise.snoise2(seed+i, 0, 4, 5.0, 0.40) * 0.75
         steps.append([round(stepsize * math.cos(direction)), round(stepsize * math.sin(direction))])
     
     if(absolute):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from helpers import dstgenerator
 from helpers import horizontalfill
 from helpers import satinstitch
 from generators import randomwalker
+from generators import wonkycircle
 import random
 from generators import dnadrawer
 
@@ -11,13 +12,17 @@ if __name__ == "__main__":
     viewer = pathviewer.PathViewer(primary_color='yellow')
     #viewer.primary_color = 'blue'
 
-    #running randomwalk + satinstitch experiment
-    path = randomwalker.randomwalker(100, stepsize=25, absolute=True)
-    satin_path = satinstitch.satinstitch(path, density=1)
+    # running circle experiment
+    seed = 1000 * random.random()
+    print(seed)
+    expath = wonkycircle.wonkycircle(500, 0.3, 0, 100, wonkiness=11, seed=seed, relative=True)
+    path = wonkycircle.wonkycircle(500, 0.3, 0, 100, wonkiness=11, seed=seed, relative=False)
+    satin_path = satinstitch.satinstitch(path, density=3)
 
-    dstgenerator.export_dst(satin_path,'experiment_output\\satinwalk7')
+    dstgenerator.export_dst(expath, 'experiment_output\\satincircle-in36')
+    dstgenerator.export_dst(satin_path,'experiment_output\\satincircle36')
 
     #viewer.render_path(path, pathviewer.DisplayMode.LINE)
-    viewer.render_path(satin_path, pathviewer.DisplayMode.LINE)
-    viewer.update()
+    # viewer.render_path(path, pathviewer.DisplayMode.LINE)
+    # viewer.update()
 
