285e8c6f365ca8b9c49c823428b37b60b0c7c8c2
davidtiemstra <dntiemstra@hotmail.com>
2025-06-04 16:37:20 +0200

    tendril sketch template

---
+++ p5/ddw_tendrils/v1/sketch.js
@@ -0,0 +1,22 @@
+let runners = [];
+let sections = [];
+
+function preload() {
+  // load font
+}
+
+function setup() {
+  // get starting runners from text
+
+  // set all to convergent mode with eachother
+
+}
+
+function draw() {
+  // step all runners (non parallel)
+
+  // step all flowers?
+
+  // export when all runners & flowers are dead
+  
+}
